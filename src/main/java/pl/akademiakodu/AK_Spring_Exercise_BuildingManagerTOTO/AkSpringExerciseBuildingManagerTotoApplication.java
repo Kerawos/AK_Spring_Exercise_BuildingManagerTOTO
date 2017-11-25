f4474a63bbcc84a8f29f35360ff281de1fd441bf
Kerawos <Maresowa@gmail.com>
2017-11-25 11:03:12 +0100

    Initial commit

---
+++ src/main/java/pl/akademiakodu/AK_Spring_Exercise_BuildingManagerTOTO/AkSpringExerciseBuildingManagerTotoApplication.java
@@ -0,0 +1,12 @@
+package pl.akademiakodu.AK_Spring_Exercise_BuildingManagerTOTO;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AkSpringExerciseBuildingManagerTotoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AkSpringExerciseBuildingManagerTotoApplication.class, args);
+	}
+}
---
+++ src/test/java/pl/akademiakodu/AK_Spring_Exercise_BuildingManagerTOTO/AkSpringExerciseBuildingManagerTotoApplicationTests.java
@@ -0,0 +1,16 @@
+package pl.akademiakodu.AK_Spring_Exercise_BuildingManagerTOTO;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AkSpringExerciseBuildingManagerTotoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
